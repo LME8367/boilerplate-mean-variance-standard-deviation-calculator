e04a5195431dd57241da48f655d2ed8402e6a319
LME8367 <lorettomunozespineira@gmail.com>
2025-12-14 22:34:29 -0300

    Developed Mean-Variance-Standard Deviation Calculator exercise

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,21 @@
 import numpy as np
 
-def calculate(list):
+def calculate(numbers):
+  if len(numbers) != 9:
+    raise ValueError("List must contain nine numbers.")
+
+  lst = np.array(numbers).reshape((3,3))
+
+  calculations = {
+    'mean': [lst.mean(axis=0).tolist(), lst.mean(axis=1).tolist(), lst.mean().tolist()],
+    'variance': [lst.var(axis=0).tolist(), lst.var(axis=1).tolist(), lst.var().tolist()],
+    'standard deviation': [lst.std(axis=0).tolist(), lst.std(axis=1).tolist(), lst.std().tolist()],
+    'max': [lst.max(axis=0).tolist(), lst.max(axis=1).tolist(), lst.max().tolist()],
+    'min': [lst.min(axis=0).tolist(), lst.min(axis=1).tolist(), lst.min().tolist()],
+    'sum': [lst.sum(axis=0).tolist(), lst.sum(axis=1).tolist(), lst.sum().tolist()]
+  }
 
 
+  return calculations
 
-
-    return calculations
+print(calculate([0,1,2,3,4,5,6,7,8]))
